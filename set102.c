4fc2eb3ec34e63fc939ea2cf29c5c23b34e6c211
sribaabu <47105404+sribaabu@users.noreply.github.com>
2019-02-11 16:12:11 +0530

    Create set102.c

---
+++ set102.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+scanf("%d",a);
+if(a>0)
+{
+if(a%2==0)
+{
+printf("it is even");
+}
+else
+{
+printf("it is odd");
+}}
+else
+{
+printf("it is invalid");
+}
+getch();
+}
