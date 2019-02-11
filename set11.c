4fdf189e64dfe82bb6c266644f4aa3262b9621c3
sribaabu <47105404+sribaabu@users.noreply.github.com>
2019-02-11 16:01:26 +0530

    Create set11.c

---
+++ set11.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+scanf("%d",a);
+if(a>0)
+{
+printf("it is positive");
+elseif(a<0)
+{
+printf("it is negative");
+}
+elseif(a==0)
+{
+printf("it is zero);
+}
+getch();
+}
