36c79fe14a76b58bbf7a0c1b7d669f884c2322a0
sribaabu <47105404+sribaabu@users.noreply.github.com>
2019-02-20 15:57:03 +0530

    Create set104.c

---
+++ set104.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+{
+char ch;
+printf("enter the alphabets");
+scanf("%d",&ch);
+if(ch=='a'||ch=='b'||ch=='c'||ch=='z')
+{
+printf("it is alphabets");
+}
+else
+{
+printf("it is not alphabet");
+}
+}
