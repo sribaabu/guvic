baeded57ea0490be0ce927307ffc7d88e6ba2f56
sribaabu <47105404+sribaabu@users.noreply.github.com>
2019-02-20 16:03:58 +0530

    Create set106.c

---
+++ set106.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+ int yr;
+ scanf("%d",&yr);
+ if((yr%4==0 && yr%100!=0) || yr%400==0)
+	{
+		printf("it isa leap year");
+	}
+	else
+	{
+		printf("itis not a leap year");
+	}
+ 
+		
+}
