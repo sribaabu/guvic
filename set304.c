675a877f281d2114194c0a4f0b954de5d384bb6b
sribaabu <47105404+sribaabu@users.noreply.github.com>
2019-03-25 15:54:10 +0530

    Create set304.c

---
+++ set304.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+int n,q,i,j,a[20];
+printf("enter the array of numbers");
+scanf("%d",&n);
+printf("enter the numbers");
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=n;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+if(a[i]>a[j])
+{
+q=a[i];
+a[i]=a[j];
+a[j]=q;
+}
+}
+}
+for(i=0;i<=n;i++)
+{
+    printf("%d",a[i]);
+}
+}
